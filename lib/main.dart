e5c0885f7ab3cfd4b62f87e91b13525af5aaa889
akio <k168577@ie.u-ryukyu.ac.jp>
2019-04-03 22:44:46 +0900

    add draw functions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui' as ui;
 
-void main() => runApp(new MyApp());
 
+void main() {
+  runApp(new MyApp());
+}
 class MyApp extends StatelessWidget {
-
-  final title = 'Flutter Sample';
-  final message = 'Sample message';
 
   @override
   Widget build(BuildContext context) {
-
     return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new MyHomePage(
-        title: this.title,
-      )
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({this.title}): super();
-
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  static final _data = [
-    Data('Apple', 200),
-    Data('Banana', 150),
-    Data('Peach', 300)
-  ];
-  Data _item;
-
-  @override
-  void initState() {
-    super.initState();
-    _item = _data[0];
-  }
-
-  void _setData() {
-    setState(() {
-      _item = (_data..shuffle()).first;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Name'),
       ),
-
-      body: Text(
-        _item.toString(),
-        style: TextStyle(fontSize:32.0),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setData,
-        tooltip: 'set message.',
-        child: Icon(Icons.star),
+      body: Center(
+        child: MyRenderBoxWidget(),
       ),
     );
   }
 }
 
-class Data {
-  int _price;
-  String _name;
-
-  Data(this._name, this._price): super();
+class MyRenderBoxWidget extends SingleChildRenderObjectWidget {
 
   @override
-  String toString() {
-    return _name + ':' + _price.toString() + 'yen';
+  RenderObject createRenderObject(BuildContext context) {
+    return _MyRenderBox();
   }
+}
+
+class _MyRenderBox extends RenderBox {
+
+  @override
+  bool hitTest(HitTestResult result, { @required Offset position }) {
+    return true;
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    Canvas c = context.canvas;
+    int dx = offset.dx.toInt();
+    int dy = offset.dy.toInt();
+
+    Paint p = Paint();
+    p.style = PaintingStyle.fill;
+    p.color = Color.fromARGB(150, 0, 200, 255);
+    Rect r = Rect.fromLTWH(dx+50.0, dy+50.0, 150.0, 150.0);
+    c.drawRect(r, p);
+
+    p.style = PaintingStyle.stroke;
+    p.color = Color.fromARGB(150, 200, 0, 255);
+    p.strokeWidth = 10.0;
+    r = Rect.fromLTWH(dx+100.0, dy+100.0, 150.0, 150.0);
+    c.drawRect(r, p);
+
+    p.style = PaintingStyle.fill;
+    p.color = Color.fromARGB(150, 0, 200, 255);
+    Offset ctr = Offset(dx+150.0, dy+150.0);
+    c.drawCircle(ctr, 75.0, p);
+
+    p.style = PaintingStyle.stroke;
+    p.strokeWidth = 5.0;
+    p.color = Color.fromARGB(150, 255, 200, 0);
+    for (var i=0; i <= 10; i++) {
+      Rect r = Rect.fromLTRB(dx+50.0+20*i, dy+50.0, dx+50.0, dy+250.0-20*i);
+      c.drawLine(r.topLeft, r.bottomRight, p);
+    }
+
+    ui.ParagraphBuilder builder = ui.ParagraphBuilder(
+      ui.ParagraphStyle(textDirection: TextDirection.ltr),
+    )
+    ..pushStyle(ui.TextStyle(color: Colors.red, fontSize: 48.0))
+    ..addText('Hello!')
+    ..pushStyle(ui.TextStyle(color: Colors.blue[700], fontSize: 48.0))
+    ..addText('This is a sample')
+    ..pushStyle(ui.TextStyle(color: Colors.blue[200], fontSize: 30.0))
+    ..addText('you an draw');
+
+    ui.Paragraph paragraph = builder.build()
+      ..layout(ui.ParagraphConstraints(width: 300.0));
+
+    Offset off = Offset(dx+50.0, dy+50.0);
+    c.drawParagraph(paragraph, off);
+  }
+
 }
